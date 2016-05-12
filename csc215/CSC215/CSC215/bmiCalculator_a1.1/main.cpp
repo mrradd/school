158d14a3bb076f954731c309510027692a89ed77
Conrad Horton <conrad.horton@gmail.com>
2016-05-12 10:15:26 -0700

    Text change, 1.2v2 added, discussion Qs for full class added
    Discussion qs are not code, and can be deleted once answered.

--- csc215/CSC215/CSC215/bmiCalculator_a1.1/main.cpp
+++ csc215/CSC215/CSC215/bmiCalculator_a1.1/main.cpp
@@ -1,5 +1,12 @@
 #include "BMICalculator.h"
 
+/**
+Conrad Horton
+CSC215 (May 2016)
+20160512
+Assignment 1.1
+**/
+
 int main(int argc, char* argv[])
   {
   BMICalculator::loop();
--- csc215/CSC215/CSC215/main.cpp
+++ csc215/CSC215/CSC215/main.cpp
@@ -1,11 +1,5 @@
 #include "a1_2/ASCIITree.h"
 
-/**
-Conrad Horton
-CSC215 (May 2016)
-2016XXXX
-Assignment XXX
-**/
 
 int main(int argc, char* argv[])
   {
