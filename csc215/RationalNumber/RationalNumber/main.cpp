163feaf2e5f94d0d769aad4819cedde4d2e1ac7b
mrradd <conrad.horton@gmail.com>
2016-06-11 18:15:49 -0700

    Getting Rational Number started

--- csc215/RationalNumber/RationalNumber/main.cpp
+++ csc215/RationalNumber/RationalNumber/main.cpp
@@ -1,21 +1,66 @@
 /******************************************************************************
 * Conrad Horton
 * CSC215 May 2016
-* 06022016
+* 06082016
 * Asgn 4.2 Rational Number
-*******************************************************************************
-* Create a class RationalNumber (fractions) with these capabilities:
-* 
-* $Create a constructor that prevents a 0 denominator in a fraction, reduces or
-*   simplifies fractions that are not in reduced form and avoids negative
-*   denominators.
-* $Overload the addition, subtraction, multiplication, and division operators
-*   for this class.
-* $Overload the relational and equality operators for this class.
 ******************************************************************************/
+
+#include <iostream>
 #include "RationalNumber.h"
+
+using namespace std;
+
+void printMenu();
+void handleMenu();
 
 int main()
   {
-  return 0;
+  handleMenu();
+  system("pause");
+  return 1;
   }
+  
+/******************************************************************************
+* handleMenu */
+/**
+* Prints the menu and handles user interaction.
+******************************************************************************/
+void handleMenu()
+  {
+  string userEntry = "";
+  
+  bool quit = false;
+  
+  do
+    {
+    printMenu();
+    getline(cin, userEntry, '\n');
+
+    cin.clear();
+    userEntry.clear();
+    }
+  while(!quit);
+  }
+  
+/******************************************************************************
+* printMenu */
+/**
+* Prints out the menu options. 
+******************************************************************************/
+void printMenu()
+  {
+  cout << "\n**********************************************************\n" << endl;
+  cout << "[1]Enter numbers into BigInt1." << endl;
+  cout << "[2]Enter numbers into BigInt2." << endl;
+  cout << "[3]Print BigInts." << endl;
+  cout << "[4]BigInt2 is 0?" << endl;
+  cout << "[5]BigInt2 is 0?" << endl;
+  cout << "[6]Compare BigInt1 to BigInt2." << endl;
+  cout << "[7]Compare BigInt2 to BigInt1." << endl;
+  cout << "[8]Sum of BigInt1 and BigInt2." << endl;
+  cout << "[9]Subtract BigInt1 from BigInt2." << endl;
+  cout << "[10]Subtract BigInt2 from BigInt1." << endl;
+  cout << "[11]Reset BigInts." << endl;
+  cout << "[Q]uit." << endl;
+  }
+  
