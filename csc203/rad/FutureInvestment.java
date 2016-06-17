1883d00c661c1d17768b93ca8fec95c07002dd1c
mrradd <conrad.horton@gmail.com>
2016-06-16 23:21:25 -0700

    Assignment 1 Future Investment class.

---
+++ csc203/rad/FutureInvestment.java
@@ -0,0 +1,22 @@
+
+package rad;
+
+/*******************************************************************************
+* Write a program that reads in investment amount, annual interest rate, and
+* number of years, and displays the future investment value using the following
+* formula:
+* futureInvestmentValue = investmentAmount * (1 + monthlyInterestRate)^(numberOfYears*12)
+*******************************************************************************/
+public class FutureInvestment
+  {
+  private double futureInvestmentValue;
+  private double investmentAmount;
+  private double monthlyInterestRate;
+  private int    numberOfYears;
+  
+  
+  public static void main(String args[])
+    {
+    }
+  }
+
