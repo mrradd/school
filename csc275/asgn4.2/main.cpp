1ab50d09a10e6bae74b41a3c48850f608effd47f
Conrad Horton <conrad.horton@gmail.com>
2016-08-13 10:47:36 -0700

    Working on asgn4.1

---
+++ csc275/asgn4.2/main.cpp
@@ -0,0 +1,26 @@
+/*******************************************************************************
+* Conrad Horton
+* CSC275
+* Asgn 4.1 Data Structures
+* 08/13/2016
+* 
+* Part 1 - Without using the STL, create a stack and queue.  Input the same set
+* of data in both.  Output the contents of both. 
+* 
+* Part 2 – Using the STL, implement a stack and a queue and complete the same
+* tasks as part 1.
+*******************************************************************************/
+#include <iostream>
+#include "Stack.h"
+int main()
+  {
+  Stack<int, 10> stack;
+  
+  //for(int i = 0; i < 11; i++)
+  //  {
+  //  int x = i;
+  //  stack.push(x);
+  //  }
+
+  return 0;
+  }
