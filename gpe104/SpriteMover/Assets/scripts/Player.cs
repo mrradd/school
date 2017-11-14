c2e6f2ecd29013ae6cd2dff64c09a06490e0d188
Conrad Horton <conrad.horton@gmail.com>
2017-11-13 19:51:39 -0800

    Sprite Mover
    Initial work on the Sprite Mover assignment.

---
+++ gpe104/SpriteMover/Assets/scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/******************************************************************************
+* Player */
+/**
+* Player controller.
+******************************************************************************/
+public class Player : MonoBehaviour
+  {
+
+  /******************************************************************************
+  * Unity methods.
+  ******************************************************************************/
+  /******************************************************************************
+  * Start
+  ******************************************************************************/
+  public void Start ()
+    {
+		
+	  }
+	
+  /******************************************************************************
+  * Update
+  ******************************************************************************/
+	public void Update ()
+    {
+    move();
+  	}
+
+  /******************************************************************************
+  * move */
+  /**
+  * Moves the player.
+  ******************************************************************************/
+  public void move()
+    {
+    Vector3 position = transform.position;
+
+    if(Input.GetKey(KeyCode.LeftArrow))
+      position.x--;
+
+    if(Input.GetKey(KeyCode.RightArrow))
+      position.x++;
+
+    if(Input.GetKey(KeyCode.UpArrow))
+      position.y++;
+
+    if(Input.GetKey(KeyCode.DownArrow))
+      position.y--;
+
+    transform.position = position;
+    }
+  }
