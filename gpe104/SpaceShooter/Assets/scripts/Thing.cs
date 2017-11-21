e08969732951fd47a3c58d72523cb0a59cf6913b
Conrad Horton <conrad.horton@gmail.com>
2017-11-20 19:39:20 -0800

    Firing bullets from ship.
    Working on spawning and moving asteroid.

---
+++ gpe104/SpaceShooter/Assets/scripts/Thing.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**************************************************************************
+* Movable */ 
+/**
+* Base class for all game assets.
+**************************************************************************/
+public abstract class Thing : MonoBehaviour
+  {
+  public float speed;
+  protected abstract void move();
+
+  }
+
