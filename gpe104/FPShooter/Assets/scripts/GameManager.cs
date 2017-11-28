743e953e589c434bba63d0f1527a89038b2e85ff
Conrad Horton <conrad.horton@gmail.com>
2017-11-27 19:43:08 -0800

    Starting project FPShooter.

---
+++ gpe104/FPShooter/Assets/scripts/GameManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+/******************************************************************************
+* GameManager */
+/**
+* Singleton class which manages the game.
+******************************************************************************/
+public class GameManager : MonoBehaviour
+  {
+  /** Instance. */ protected static GameManager mInstance;
+
+  public static int click = 0;
+
+  /** Returns an instance of this. */
+  public static GameManager instance
+    {
+    get
+      {
+      if(mInstance == null)
+        mInstance = new GameManager();
+
+      return mInstance;
+      }
+    }
+    
+  protected GameManager(){}
+  
+  /**************************************************************************
+  * Unity Methods 
+  **************************************************************************/
+  /**************************************************************************
+  * Start */ 
+  /**
+  **************************************************************************/
+  public void Awake ()
+    {
+    mInstance = this;
+    }
+
+  /**************************************************************************
+  * Update */ 
+  /**
+  **************************************************************************/
+  public void Update ()
+    {
+    if(Input.GetKeyDown(KeyCode.A))
+      Debug.Log(click);
+    }
+
+  /**************************************************************************
+  * Methods 
+  **************************************************************************/
+  /**************************************************************************
+  * loadMainGameScene */ 
+  /**
+  * Loads the Main Game Scene.
+  **************************************************************************/
+  public void loadMainGameScene()
+    {
+    SceneManager.LoadScene("MainGameScene", LoadSceneMode.Single);
+    click+=20;
+    }
+
+  /**************************************************************************
+  * testButton */ 
+  /**
+  * Loads the Main Game Scene.
+  **************************************************************************/
+  public void testButton()
+    {
+    click++;
+    }
+  }
